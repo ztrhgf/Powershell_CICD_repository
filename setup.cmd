e8387732d3bb8b6b2f63501e0f5ebe149ef3fcea
ztrhgf <ondrejs4@kentico.com>
2021-04-06 19:23:19 +0200

    run PS maximized so all install options are visible

--- setup.cmd
+++ setup.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
 
-%WINDIR%\System32\WindowsPowerShell\v1.0\powershell.exe -executionPolicy bypass -noprofile -noexit -file stp.ps1
+%WINDIR%\System32\WindowsPowerShell\v1.0\powershell.exe -executionPolicy bypass -noprofile -noexit -windowStyle maximized -file stp.ps1
